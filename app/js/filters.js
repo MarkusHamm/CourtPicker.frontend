6c527275a47c62f88585de5b575037fb0b53b87b
Markus Hamm <markus.hamm@gmail.com>
2014-08-03 09:00:14 +0200

    enhanced userfilter

--- app/js/filters.js
+++ app/js/filters.js
@@ -14,20 +14,28 @@ angular.module('myApp.filters', [])
       var result = [];
 
       var filterStr = filterStr.replace(/\,/gi, ''); // remove , from input string
+      filterStr = filterStr.toLowerCase();
       var filters = filterStr.split(' '); // extract filter parts
 
       for (var i= 0; i<input.length; i++) {
         var user = input[i];
         var matchCount = 0;
+
         for (var j=0; j<filters.length; j++) {
           var filter = filters[j];
-          if (user.firstName.indexOf(filter) != -1 ||
-              user.lastName.indexOf(filter) != -1 ||
-              user.userName.indexOf(filter) != -1 ||
-              user.email.indexOf(filter) != -1) {
+          var firstName = user.firstName ? user.firstName.toLowerCase() : '';
+          var lastName = user.lastName ? user.lastName.toLowerCase() : '';
+          var userName = user.userName ? user.userName.toLowerCase() : '';
+          var email = user.email ? user.email.toLowerCase() : '';
+
+          if (firstName.indexOf(filter) != -1 ||
+              lastName.indexOf(filter) != -1 ||
+              userName.indexOf(filter) != -1 ||
+              email.indexOf(filter) != -1) {
             matchCount++;
           }
         }
+
         if (matchCount == filters.length) {
           result.push(user);
         }
